f539514ead564626d9e8b1c05917f5ce9f558b55
Ritzz <154762212+RitzzRiver@users.noreply.github.com>
2024-07-09 01:08:36 -0500

    init

---
+++ src/Ops/Operations.java
@@ -0,0 +1,38 @@
+package Ops;
+
+/**
+ * @author Kris
+ */
+public class Operations {
+
+    public static int Addition(int num1, int num2){
+        int sum;
+
+        sum = num1 + num2;
+
+        return sum;
+    }
+    
+    public static int Substract(int num1, int num2){
+        int substract;
+
+        substract = num1 - num2;
+
+        return substract;
+    }
+
+    public static int Multiply(int num1, int num2){
+        int product;
+
+        product = num1 * num2;
+
+        return product;
+    }
+
+    public static int Divide(int num1, int num2){
+        int product;
+        product = num1 / num2;
+        return product;
+    }
+
+}
